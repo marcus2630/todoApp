08c64e54f74cefeb7f8e08c314ffe58fa4c9ccc6
Marcus Klausen <marcusklausen@gmail.com>
2019-01-22 18:09:30 +0100

    Initial Commit

---
+++ toDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toDo
+//
+//  Created by marcus on 22/01/2019.
+//  Copyright © 2019 marcusklausen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
